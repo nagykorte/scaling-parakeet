df64a2e9019482af93dbfca4f69d740dca60aa10
nagykorte <nicolasmatray@hotmail.com>
2021-06-24 02:37:36 -0300

    asd

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -22,27 +22,30 @@ const Tabs = () => {
     });
   }
 
-  const handler = React.useCallback(
-    () => {
-      // Update coordinates
-      if (document.documentElement.scrollTop > 1269) {
-        setPosition(3)
-      } else if (document.documentElement.scrollTop > 634) {
-        setPosition(2)
-      } else {
-        setPosition(1)
-      }
-
-      // setCoords({ x: clientX, y: clientY });
-    },
-    [setPosition]
-  );
-
-  // Add event listener using our hook
-  React.useEventListener('scroll', handler);
-
-
-
+  // const handler = React.useCallback(
+  //   () => {
+  //     // Update coordinates
+  
+  //     // setCoords({ x: clientX, y: clientY });
+  //   },
+  //   [setPosition]
+  // );
+  
+  // // Add event listener using our hook
+  // React.useEventListener('scroll', handler);
+  
+  
+  if (typeof document !== 'undefined') {
+    React.useEffect(()=> {
+                if (document.documentElement.scrollTop > 1269) {
+            setPosition(3)
+          } else if (document.documentElement.scrollTop > 634) {
+            setPosition(2)
+          } else {
+            setPosition(1)
+          }
+  }, [document.documentElement.scrollTop])
+}
 
 
 
@@ -64,47 +67,47 @@ const Tabs = () => {
     <div style={{ width: '3vw', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '1vw', position: 'fixed' }}>
       <FontAwesomeIcon icon={position === 1 ? faCircle : faCircleNotch} onClick={() => { scrollToPage(0) }} />
       <br></br>
-      <FontAwesomeIcon icon={position === 2 ? faCircle : faCircleNotch} onClick={()=>{scrollToPage(1)}} />
+      <FontAwesomeIcon icon={position === 2 ? faCircle : faCircleNotch} onClick={() => { scrollToPage(1) }} />
       <br></br>
       <FontAwesomeIcon icon={position === 3 ? faCircle : faCircleNotch} onClick={() => { scrollToPage(2) }} />
     </div>
   )
 }
 
-// Hook
-function useEventListener(eventName, handler, element = document) {
-  // Create a ref that stores handler
-  const savedHandler = React.useRef();
+// // Hook
+// function useEventListener(eventName, handler, element = document) {
+//   // Create a ref that stores handler
+//   const savedHandler = React.useRef();
 
-  // Update ref.current value if handler changes.
-  // This allows our effect below to always get latest handler ...
-  // ... without us needing to pass it in effect deps array ...
-  // ... and potentially cause effect to re-run every render.
-  React.useEffect(() => {
-    savedHandler.current = handler;
-  }, [handler]);
+//   // Update ref.current value if handler changes.
+//   // This allows our effect below to always get latest handler ...
+//   // ... without us needing to pass it in effect deps array ...
+//   // ... and potentially cause effect to re-run every render.
+//   React.useEffect(() => {
+//     savedHandler.current = handler;
+//   }, [handler]);
 
-  React.useEffect(
-    () => {
-      // Make sure element supports addEventListener
-      // On
-      const isSupported = element && element.addEventListener;
-      if (!isSupported) return;
+//   React.useEffect(
+//     () => {
+//       // Make sure element supports addEventListener
+//       // On
+//       const isSupported = element && element.addEventListener;
+//       if (!isSupported) return;
 
-      // Create event listener that calls handler function stored in ref
-      const eventListener = (event) => savedHandler.current(event);
+//       // Create event listener that calls handler function stored in ref
+//       const eventListener = (event) => savedHandler.current(event);
 
-      // Add event listener
-      element.addEventListener(eventName, eventListener);
+//       // Add event listener
+//       element.addEventListener(eventName, eventListener);
 
-      // Remove event listener on cleanup
-      return () => {
-        element.removeEventListener(eventName, eventListener);
-      };
-    },
-    [eventName, element] // Re-run if eventName or element changes
-  );
-}
+//       // Remove event listener on cleanup
+//       return () => {
+//         element.removeEventListener(eventName, eventListener);
+//       };
+//     },
+//     [eventName, element] // Re-run if eventName or element changes
+//   );
+// }
 
 
 export default Tabs
